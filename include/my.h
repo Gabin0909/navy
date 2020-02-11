fde9633c6415db68586ec3b95945e7a664e0ccc9
Jad CHAHED <jad.chahed@epitech.eu>
2020-02-11 16:24:44 +0100

    [ADD] modified Makefile main and my.h

---
+++ include/my.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2018
+** my.h
+** File description:
+** header
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdarg.h>
+
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+void my_putchar(char);
+int my_put_nbr(int);
+int my_putstr(char const *);
+int my_strlen(char const *);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_navy_2019
+** File description:
+** main.c
+*/
+
+#include "my.h"
+
+int main(int argc, char **argv)
+{
+    return (navy(argc, argv));
+}
