d914d8bc5a4962705bbc73c301b7227d684c8064
Ulysse Queritet-Diop <le-ul@hotmail.fr>
2020-02-18 14:23:36 +0100

    [Fix] usage

--- src/usage.c
+++ src/usage.c
@@ -13,7 +13,7 @@ int usage(int argc, char **argv)
         my_putstr("USAGE\n");
         my_putstr("      ./navy [first_player_pid] navy_positions\n");
         my_putstr("Description\n");
-        my_putstr("     first_player_pid:  only for the 2nd player. pid pf"):
+        my_putstr("     first_player_pid:  only for the 2nd player. pid pf");
         my_putstr(" the first player\n");
         my_putstr("navy_positions:  fiel representing the positions of the");
         my_putstr(" ships.\n");
