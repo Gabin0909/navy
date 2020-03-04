f66f2f51d0f995c281f59fd89880f0523df6441f
Ulysse Queritet-Diop <le-ul@hotmail.fr>
2020-03-05 00:31:06 +0100

    [FIXED] check if already hit and missed

--- src/already_hit.c
+++ src/already_hit.c
@@ -21,13 +21,13 @@ int already_hit(int argc, info_t *info)
     if (argc == 2) {
         for (; info->p1_enemy_map[0][j] != info->atk_pos[0]; j++);
         for (; info->p1_enemy_map[i][0] != info->atk_pos[1]; i++);
-        if (info->p1_enemy_map[i][j] == 'x')
+        if (info->p1_enemy_map[i][j] == 'x' || info->p1_enemy_map[i][j] == 'o')
             return (1);
     }
     else {
         for (; info->p2_enemy_map[0][j] != info->atk_pos[0]; j++);
         for (; info->p2_enemy_map[i][0] != info->atk_pos[1]; i++);
-        if (info->p2_enemy_map[i][j] == 'x')
+        if (info->p2_enemy_map[i][j] == 'x' || info->p1_enemy_map[i][j] == 'o')
             return (1);
     }
     return (0);
